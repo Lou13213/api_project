c36ba2d855ebc20f1d74a63a6bbfff31894a82b1
Lou13213 <67229851+Lou13213@users.noreply.github.com>
2022-12-15 10:26:52 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AUPHAN_BOISSON_GALLET_VELLY_AVIATION';
+}
